da2dc6acf8a23ce60e7050710bd77e2fbd70f12c
ToshihiroOgino <tonigo0725@gmail.com>
2025-05-25 13:43:17 +0900

    fix: improve script usage messages and streamline file handling

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,34 +1,36 @@
 #!/bin/bash
 TARGET=$1
-if [ -z "$TARGET" ]; then
-    echo "Usage: $0 <target>"
+FILES=$(find ./setuptools/tests -maxdepth 1 -name '*.c' -type f | xargs -I x basename x)
+
+if [ -z "$TARGET" ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+    echo "Usage:"
+    echo "  $0 <target>"
+    echo "  $0 --list(-l)"
+    echo "  $0 --all(-a)"
     exit 0
-elif [ "$1" = "-h" ]; then
+elif [ "$1" = "-l" ] || [ "$1" = "--list" ]; then
     echo "Usage: $0 <target>"
     echo "available targets:"
-    FILES=$(find ./setuptools/tests -maxdepth 1 -name '*.c' -type f | xargs -I x basename x)
-    for FILE in $(sort <<< "$FILES"); do
+    for FILE in $(sort <<<"$FILES"); do
         echo "  - $FILE"
+    done
+    exit 0
+elif [ "$1" = "--all" ] || [ "$1" = "-a" ]; then
+    for FILE in $(sort <<<"$FILES"); do
+        TARGET=${FILE}
+        echo "Testing $TARGET"
+        ./scripts/test.sh $TARGET
     done
     exit 0
 fi
 
 BIN_FILE="./test/binary/$TARGET.out"
-if [ -f $BIN_FILE ]; then
-    rm $BIN_FILE
-fi
-/usr/local/core/bin/m2cc -.o ./setuptools/tests/$TARGET -o $BIN_FILE
+/usr/local/core/bin/m2cc -.o ./setuptools/tests/$TARGET -o $BIN_FILE 2>/dev/null
 
 EXPECTED_FILE="./test/mmvm/$TARGET.out.txt"
-if [ -f $EXPECTED_FILE ]; then
-    rm $EXPECTED_FILE
-fi
 /usr/local/core/bin/mmvm -d $BIN_FILE 2>$EXPECTED_FILE
 
 RES_FILE="./test/res/$TARGET.out.txt"
-if [ -f $RES_FILE ]; then
-    rm $RES_FILE
-fi
-cargo run -- -d $BIN_FILE >>$RES_FILE
+cargo run -- -d $BIN_FILE >$RES_FILE
 
 diff -i $RES_FILE $EXPECTED_FILE
